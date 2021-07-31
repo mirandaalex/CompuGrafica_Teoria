0abee1faf221ee7d74073585489b49f347112501
mirandaalex <amiranda.5_1997@hotmail.com>
2021-07-30 22:56:07 -0500

    Se agregó UI

--- Proyecto_CG/Programa/Proyecto/CGEIHC-ProyectoFinal/CGEIHC-ProyectoFinal/CGEIHC-ProyectoFinal.cpp
+++ Proyecto_CG/Programa/Proyecto/CGEIHC-ProyectoFinal/CGEIHC-ProyectoFinal/CGEIHC-ProyectoFinal.cpp
@@ -19,15 +19,19 @@
 
 // irrKlang sound library
 #include <irrKlang.h>
-
+// Text library
+#include <ft2build.h>
+#include FT_FREETYPE_H  
 // Model loading classes
 #include <shader_m.h>
 #include <camera.h>
 #include <model.h>
 #include <Light.h>
 #include <Material.h>
-
+//#include <Text.h>
 #include <iostream>
+
+
 
 // Definición de callbacks
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -66,10 +70,10 @@ float escalamientoGlobo = 1.0f;
 float desplazamientoGlobo;
 float tiempo = 0.0f;
 
-float velocidadPuerta = 110.0f / 3.0f;
-float velocidadVentanas = 1.215f / 2.0f;
-float velocidadTapaInodoro = 90.0f / 2.0f;
-float velocidadGlobo = 1.0f / 4.0f;
+float velocidadPuerta = 0.4f ;
+float velocidadVentanas = 0.02f ;
+float velocidadTapaInodoro = 0.4f ;
+float velocidadGlobo = 0.05f ;
 
 // Fuentes de luz
 Light sun = Light(glm::vec3(0.0f,100.0f,0.0f), glm::vec3(0.0f,-1.0f,0.0f), glm::vec4(1.0f,1.0f,1.0f,1.0f), glm::vec4(100.0f,100.0f,100.0f,100.0f),1,20.f);
@@ -111,6 +115,20 @@ float grassTime;
 bool walking = false;
 
 
+//Texto
+void renderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color, glm::mat4 projection);
+unsigned int VAO_text, VBO_text;
+float zoom = 0;
+struct Character {
+	unsigned int TextureID;  // ID handle of the glyph texture
+	glm::ivec2   Size;       // Size of glyph
+	glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+	unsigned int Advance;    // Offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
+
+
 // Entrada a función principal
 
 int main()
@@ -149,11 +167,103 @@ int main()
 	// Activación de buffer de profundidad
 	glEnable(GL_DEPTH_TEST);
 
+	
 	// Compilación y enlace de shaders
 	Shader ourShader("shaders/10_vertex_skinning-IT.vs", "shaders/10_fragment_skinning-IT.fs");
 	Shader cubemapShader("shaders/10_vertex_cubemap.vs", "shaders/10_fragment_cubemap.fs");
 	Shader basicPhongShader("shaders/11_BasicPhongShader.vs", "shaders/11_BasicPhongShader.fs"); // opacos y plásticos, cerámicas
 	Shader fresnelShader("shaders/11_Fresnel.vs", "shaders/11_Fresnel.fs"); // metal y semitraslúcidos
+	Shader textShader("shaders/text.vs","shaders/text.fs");
+	
+	//Config Texto
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
+	textShader.use();
+	glUniformMatrix4fv(glGetUniformLocation(textShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	// FreeType
+	// --------
+	FT_Library ft;
+	// All functions return a value different than 0 whenever an error occurred
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+		return -1;
+	}
+
+	// find path to font
+	/*std::string font_name = FileSystem::getPath("resources/fonts/Antonio-Bold.ttf");
+	if (font_name.empty())
+	{
+		std::cout << "ERROR::FREETYPE: Failed to load font_name" << std::endl;
+		return -1;
+	}*/
+
+	// load font as face
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face)) {
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+		return -1;
+	}
+	else {
+		// set size to load glyphs as
+		FT_Set_Pixel_Sizes(face, 0, 48);
+
+		// disable byte-alignment restriction
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		// load first 128 characters of ASCII set
+
+		for (unsigned char c = 0; c < 128; c++)
+		{
+			// Load character glyph 
+			if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+			{
+				std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+				continue;
+			}
+			// generate texture
+			unsigned int texture;
+			glGenTextures(1, &texture);
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glTexImage2D(
+				GL_TEXTURE_2D,
+				0,
+				GL_RED,
+				face->glyph->bitmap.width,
+				face->glyph->bitmap.rows,
+				0,
+				GL_RED,
+				GL_UNSIGNED_BYTE,
+				face->glyph->bitmap.buffer
+			);
+			// set texture options
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			// now store character for later use
+			Character character = {
+				texture,
+				glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+				glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+				static_cast<unsigned int>(face->glyph->advance.x)
+			};
+			Characters.insert(std::pair<char, Character>(c, character));
+		}
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	// destroy FreeType once we're finished
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	glGenVertexArrays(1, &VAO_text);
+	glGenBuffers(1, &VBO_text);
+	glBindVertexArray(VAO_text);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_text);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// Máximo número de huesos: 100
 #define MAX_RIGGING_BONES 100
@@ -668,7 +778,13 @@ int main()
 			ufo.Draw(fresnelShader);
 
 		}
-
+		glUseProgram(0);
+		//TEXTO
+		{
+			renderText(textShader, "Camara modo: "+std::to_string(cameraMode), 10.0f, 10.0f, 0.5f, glm::vec3(0.1f, 0.1f, 0.1f),projection);
+			zoom=45.0f - camera.Zoom;
+			renderText(textShader, "zoom: "+std::to_string(int(zoom)), 750.0f, 10.0f, 0.5f, glm::vec3(0.1, 0.1f, 0.1f), projection);
+		}
 		glUseProgram(0);
 		{
 			ufoAngle += 4;
@@ -841,49 +957,49 @@ void processInput(GLFWwindow* window)
 
 	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
 	{
-		rotacionPuertas -= elapsedTime*velocidadPuerta;
+		rotacionPuertas -= velocidadPuerta;
 		if (rotacionPuertas < -100.0f)
 			rotacionPuertas = -100.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
 	{
-		rotacionPuertas += elapsedTime*velocidadPuerta;
+		rotacionPuertas += velocidadPuerta;
 		if (rotacionPuertas > 0.0f)
 			rotacionPuertas = 0.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
 	{
-		rotacionTapaInodoro -= elapsedTime*velocidadTapaInodoro;
+		rotacionTapaInodoro -= velocidadTapaInodoro;
 		if (rotacionTapaInodoro < -90.0f)
 			rotacionTapaInodoro = -90.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
 	{
-		rotacionTapaInodoro += elapsedTime*velocidadTapaInodoro;
+		rotacionTapaInodoro += velocidadTapaInodoro;
 		if (rotacionTapaInodoro > 0.0f)
 			rotacionTapaInodoro = 0.0f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
 	{
-		traslacionVentanas -= elapsedTime*velocidadVentanas;
+		traslacionVentanas -= velocidadVentanas;
 		if (traslacionVentanas < -0.55f)
 			traslacionVentanas = -0.55f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
 	{
-		traslacionVentanas += elapsedTime*velocidadVentanas;
+		traslacionVentanas += velocidadVentanas;
 		if (traslacionVentanas > 0.665f)
 			traslacionVentanas = 0.665f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
 	{
-		escalamientoGlobo -= elapsedTime * velocidadGlobo;
+		escalamientoGlobo -= velocidadGlobo;
 		if (escalamientoGlobo < 0.1f)
 			escalamientoGlobo = 0.1f;
 	}
 	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
 	{
-		escalamientoGlobo += elapsedTime * velocidadGlobo;
+		escalamientoGlobo += velocidadGlobo;
 		if (escalamientoGlobo > 1.0f)
 			escalamientoGlobo = 1.0f;
 	}
@@ -1001,4 +1117,49 @@ unsigned int loadCubemap(vector<std::string> faces)
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
 	return textureID;
+}
+
+void renderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color, glm::mat4 projection) {
+	// activate corresponding render state	
+	shader.use();
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+	glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(VAO_text);
+
+	// iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		float xpos = x + ch.Bearing.x * scale;
+		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		float w = ch.Size.x * scale;
+		float h = ch.Size.y * scale;
+		// update VBO_text for each character
+		float vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos,     ypos,       0.0f, 1.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+			{ xpos + w, ypos + h,   1.0f, 0.0f }
+		};
+		// render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// update content of VBO_text memory
+		glBindBuffer(GL_ARRAY_BUFFER, VBO_text);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
